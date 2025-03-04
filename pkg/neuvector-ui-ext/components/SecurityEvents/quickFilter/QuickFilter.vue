373c53705132e292e2c8bd039ebe50fe81566f90
Steven Zhang <steven.zhang@suse.com>
2025-03-03 19:18:23 -0800

    NVSHAS-9850: [UI-Ext] All quick filters do not work after Vue3 migration.

--- pkg/neuvector-ui-ext/components/SecurityEvents/quickFilter/QuickFilter.vue
+++ pkg/neuvector-ui-ext/components/SecurityEvents/quickFilter/QuickFilter.vue
@@ -21,6 +21,6 @@
 
 <template>
     <div class="search row">
-        <input id="quick-filter" type="text" placeholder="Filter" class="input-sm search-box" :value="filterText" @keyup="filterByQuickFilter($event, filterText)"/>
+        <input id="quick-filter" type="text" placeholder="Filter" class="input-sm search-box" v-model="filterText" @keyup="filterByQuickFilter($event, filterText)"/>
     </div>
 </template>
