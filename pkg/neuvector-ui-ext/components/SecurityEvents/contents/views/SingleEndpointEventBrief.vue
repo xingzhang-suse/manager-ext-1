e11721ab51ee9689fa86028f53e7012406991485
Steven Zhang <steven.zhang@suse.com>
2026-02-12 21:10:35 -0800

    #187 [Bug] The list style of Security event page is broken

--- pkg/neuvector-ui-ext/components/SecurityEvents/contents/views/SingleEndpointEventBrief.vue
+++ pkg/neuvector-ui-ext/components/SecurityEvents/contents/views/SingleEndpointEventBrief.vue
@@ -202,3 +202,9 @@
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+    @import '../../../../styles/bootstrap/scss/bootstrap.scss';
+    @import '../../../../styles/security-events.scss';
+    @import '../../../../styles/neuvector.scss';
+</style>
